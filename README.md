2a9a64c19e9002d6e42a4d6fb965d1873f463f38
joCol-git <joao.r.colaco@gmail.com>
2023-04-07 20:22:09 +0100

    all

---
+++ README.md
@@ -0,0 +1,12 @@
+# Getting Started with Create React App
+
+
+docker run -it -p 3000:3000 -v /app/node_modules -v ~/frontend:/app 1cea7d38c0782faa89149cab
+
+docker run -it -p 3000:3000 1cea7d38c0782faa89149cab sh
+
+docker build -f Dockerfile.dev -t username:jcolaco .
+
+docker ps
+
+explorer.exe, code .,
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hi there 222 sas
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
